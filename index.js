a6238579976b3a8533e221c25090c5c0568908d2
internet_trash <naiknischal92@gmail.com>
2024-05-14 09:29:02 +0530

    add cors

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import cors from 'cors';
+
+const  app = express();
+
+// Use cors middleware
+app.use(cors());
+
+// Your routes go here
 
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 
-const app = express();
+app.use(cors());
 //endpooit 
 app.get('/convert', async (req, res) => {
     const { url, numPages } = req.query;
